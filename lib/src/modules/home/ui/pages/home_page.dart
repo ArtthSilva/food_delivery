1ab7625f02d1b75d0df9a2c590340f6200396b60
ArtthSilva <arthur12pereira1@gmail.com>
2024-02-22 16:53:52 -0300

    fix: corrigindo bug dos widgets quebrar com teclado ativado

--- lib/src/modules/home/ui/pages/home_page.dart
+++ lib/src/modules/home/ui/pages/home_page.dart
@@ -10,133 +10,147 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
-   late TabController tabController;
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController tabController;
 
   @override
   void initState() {
     tabController = TabController(initialIndex: 0, length: 4, vsync: this);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-         body: Stack(children: [
-           Align(
-             alignment: const Alignment(-0.75, -0.85),
-             child: InkWell(
-               child: Image.asset('assets/images/menu_icon.png'),
-               onTap: () => ZoomDrawer.of(context)!.toggle(),
-             ),
-           ),
-           Align(
-               alignment: const Alignment(0.9, -0.9),
-               child: IconButton(
-                   onPressed: () {},
-                   icon: const Icon(
-                     Icons.shopping_cart_outlined,
-                     color: Colors.grey,
-                   ))),
-           Align(
-             alignment: const Alignment(-0.65, -0.7),
-             child: Text(
-               'Delicious',
-               style: GoogleFonts.balooTamma2(
-                 fontWeight: FontWeight.w700,
-                 fontSize: 34,
-               ),
-             ),
-           ),
-           Align(
-             alignment: const Alignment(-0.55, -0.6),
-             child: Text(
-               'food for you',
-               style: GoogleFonts.balooTamma2(
-                 fontWeight: FontWeight.w700,
-                 fontSize: 34,
-               ),
-             ),
-           ),
-           Padding(
-             padding: const EdgeInsets.only(left: 30, right: 30),
-             child: SearchBar(
-               backgroundColor: MaterialStateProperty.all(Colors.grey[100]),
-               leading: const Icon(Icons.search),
-               hintText: 'Search',
-             ),
-           ),
-           Align(
-             alignment: Alignment.bottomCenter,
-             child: SizedBox(
-               width: MediaQuery.sizeOf(context).width,
-               height: MediaQuery.of(context).size.height / 1.8,
-               child: Column(
-                 children: [
-                   Padding(
-                     padding: const EdgeInsets.only(left: 26),
-                     child: TabBar(
-                       tabAlignment: TabAlignment.start,
-                       isScrollable: true,
-                       controller: tabController,
-                       labelColor: Colors.black,
-                       unselectedLabelColor: Colors.grey,
-                       indicatorColor: const Color.fromARGB(234, 255, 84, 68),
-                       tabs: const [
-                         Padding(
-                           padding: EdgeInsets.only( bottom: 5),
-                           child: Text(
-                             'Foods',
-                             style: TextStyle(
-                               fontSize: 17,
-                             ),
-                           ),
-                         ),Padding(
-                           padding: EdgeInsets.only(right: 12.0, bottom: 5),
-                           child: Text(
-                             'Drinks',
-                             style: TextStyle(
-                               fontSize: 17,
-                             ),
-                           ),
-                         ),Padding(
-                           padding: EdgeInsets.only(right: 12.0, bottom: 5),
-                           child: Text(
-                             'Snacks',
-                             style: TextStyle(
-                               fontSize: 17,
-                             ),
-                           ),
-                         ),
-                         Padding(
-                           padding: EdgeInsets.only(right: 12.0, bottom: 5),
-                           child: Text(
-                             'Frutas',
-                             style: TextStyle(
-                               fontSize: 17,
-                             ),
-                           ),
-                         ),
-                       ],
-                     ),
-                   ),
-                   Expanded(
-                       child: TabBarView(
-                     controller: tabController,
-                     children: [
-                       ListView(
-                         scrollDirection: Axis.horizontal,
-                         children: const [
-                           CardProduct(),CardProduct(),CardProduct(),]),
-                           Container(color: Colors.green,),
-                           Container(color: Colors.blue,),
-                           Container(color: Colors.orange,),
-                         
-                        
-                     ],
-                   )),
-                   BottomNavigationBar(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Column(children: [
+              const SizedBox(
+                height: 55,
+              ),
+              Align(
+                alignment: const Alignment(-0.75, 1),
+                child: InkWell(
+                  child: Image.asset('assets/images/menu_icon.png'),
+                  onTap: () => ZoomDrawer.of(context)!.toggle(),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Row(
+                children: [
+                  const SizedBox(width: 33),
+                  Text(
+                    'Delicious \nfood for you',
+                    style: GoogleFonts.balooTamma2(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 34,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 30, right: 30, top: 15, bottom: 45),
+                child: SearchBar(
+                  backgroundColor: MaterialStateProperty.all(Colors.grey[100]),
+                  leading: const Icon(Icons.search),
+                  hintText: 'Search',
+                ),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 26),
+                    child: TabBar(
+                      tabAlignment: TabAlignment.start,
+                      isScrollable: true,
+                      controller: tabController,
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Colors.grey,
+                      indicatorColor: const Color.fromARGB(234, 255, 84, 68),
+                      tabs: const [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 5),
+                          child: Text(
+                            'Foods',
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 12.0, bottom: 5),
+                          child: Text(
+                            'Drinks',
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 12.0, bottom: 5),
+                          child: Text(
+                            'Snacks',
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 12.0, bottom: 5),
+                          child: Text(
+                            'Frutas',
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.sizeOf(context).width,
+                    height: MediaQuery.sizeOf(context).height * 0.43,
+                    child: TabBarView(controller: tabController, children: [
+                      ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: const [
+                          CardProduct(),
+                          CardProduct(),
+                          CardProduct(),
+                        ],
+                      ),
+                      ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: const [
+                          CardProduct(),
+                          CardProduct(),
+                          CardProduct(),
+                        ],
+                      ),
+                      ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: const [
+                          CardProduct(),
+                          CardProduct(),
+                          CardProduct(),
+                        ],
+                      ),
+                      ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: const [
+                          CardProduct(),
+                          CardProduct(),
+                          CardProduct(),
+                        ],
+                      ),
+                    ]),
+                  ),
+                                     BottomNavigationBar(
                      showSelectedLabels: false,
                      showUnselectedLabels: false,
                      items: const [
@@ -144,12 +158,13 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
                      BottomNavigationBarItem(icon: Icon(Icons.favorite_border),label: 'asdsa'),
                      BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined),label: 'dasda'),
                    ])
-                 ],
-               ),
-             ),
-           )
-         ]),
+                ],
+              ),
+
+            ]),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
